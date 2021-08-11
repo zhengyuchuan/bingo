ecd52f0a9a5cd41c5d83e5b1654fef29820ca038
zhengyuchuan <1002086995@qq.com>
2021-08-11 15:48:09 +0800

    add: mysqldb

---
+++ dao/mysqlDB.go
@@ -0,0 +1,37 @@
+package dao
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql" // mysql驱动
+	"gorm.io/gorm"
+	"time"
+)
+
+var db *gorm.DB
+
+func Init() {
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("数据库初始化失败！")
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		fmt.Println("数据库连接池化失败！")
+	}
+
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+}
+
+func getMysqlDB() *gorm.DB {
+	return db
+}
--- go.mod
+++ go.mod
@@ -10,4 +10,6 @@ require (
 	github.com/spf13/viper v1.8.1
 	go.uber.org/zap v1.17.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.12
 )
